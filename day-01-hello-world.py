1b143ddfa38fad08cacc86f36b061e0212e8ac27
devadi2912 <adityapramanik2021@gmail.com>
2026-05-13 16:54:27 +0530

    new change

--- day-01-hello-world.py
+++ day-01-hello-world.py
@@ -1,2 +1,9 @@
 # this is my first git commit and the first python file that in recording on git hub . 
 print("hello world this is my first python commit to git !!")
+
+# taking in imput and then printing the numbers form one till the limit 
+a= int (input ("enter the limit :"))
+for i in range (a+1):
+    print (i)
+    
+print ("the end !")
